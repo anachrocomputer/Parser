abe186f1161c76a7c1e18f007f22787bcb5786fa
John Honniball <coredump@virginmedia.com>
2022-08-22 21:02:42 +0100

    Add test source file for simple declarations.
    No errors, no arrays, no storage classes, no type modifiers. Functions with no arguments lists or bodies.

---
+++ simpledecl.c
@@ -0,0 +1,30 @@
+/* Some simple declarations which the code so far is capable of parsing */
+
+// Scalars
+char Tea;
+int Sugar;
+float Rum;
+double Whale;
+
+// Initialised scalars
+char Ship = 'T';
+int Sailors = 42;
+float Soon = 12.0f;
+double Cargo = 153e4;
+
+// Pointers
+void *Sail = 0;
+char **Billy = 0;
+int *Heave = 0;
+float **Rope = 0;
+double *Anchor = 0;
+
+// Functions
+void PutToSea();
+char SeenWhale();
+int Blow();
+float Buoyancy();
+double ConsumeRum();
+
+char *NameOfTheShip();
+
