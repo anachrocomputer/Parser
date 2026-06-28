f216d9246adcdf85d33298c7d07f4b372e83df3c
John Honniball <coredump@virginmedia.com>
2026-06-29 00:16:16 +0100

    Run subprocesses for compiler, assembler, and simulator.
    TODO: extract expected output from comments in source code, and compare to actual output.

--- test/runner.py
+++ test/runner.py
@@ -1,17 +1,44 @@
 # runner --- run all the C compiler tests                      2026-06-21
 
 import os
+import subprocess
 import glob
 
 def runtest(testClass, name, path):
     print("Running '" + testClass + "', test '" + name + "' in '" + path + "'")
-    cc = "../parser " + path + ".c"
-    asm = "asm6809 --6309 " + path + ".asm"
-    sim = "sim6809 -n -g -q " + path + ".hex"
-    print(cc)
+    src = path + ".c"
+    asm = path + ".asm"
+    lst = path + ".lst"
+    hex = path + ".hex"
+
+    args = ["../parser", src]
+    print(" ".join(args))
+
+    with subprocess.Popen(args, stdout=subprocess.PIPE) as compiler:
+       msgs = compiler.stdout.read()
+    
+    print("Compiler output: '" + msgs.decode("utf-8") + "'")
+    
     # check here for a file called 'core'
-    print(asm)
-    print(sim)
+
+    args = ["/home/john/bin/asm6809", "--6809", "-H", "-o", hex, "-l", lst, asm]
+    print(" ".join(args))
+    
+    with subprocess.Popen(args, stdout=subprocess.PIPE) as assembler:
+       msgs = assembler.stdout.read()
+    
+    print("Assembler output: '" + msgs.decode("utf-8") + "'")
+
+    args = ["/home/john/bin/sim6809", "-n", "-g", "-q", hex]
+    print(" ".join(args))
+
+    with subprocess.Popen(args, stdout=subprocess.PIPE) as simulator:
+       msgs = simulator.stdout.read()
+    
+    print("Simulator output: '" + msgs.decode("utf-8") + "'")
+
+    input("Press Enter to continue...")
+
 
 for d in os.listdir():
     if os.path.isfile(d):
